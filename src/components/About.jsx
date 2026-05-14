c2d7e3335ba5e5f837d72547a62934a86bb8014b
Andrew Hing <andrew@andrews-mbp.lan>
2026-05-14 09:28:06 -0400

    updated details

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,9 +16,10 @@ export const About = () => {
         I first started in tech when I was kid where I would make and code robots out of legos.
         This sparked my interest in programming and problem-solving, leading me to pursue a career in software development.
         I am currently a Masters student at Stevens Institute of Technology, where I am skilled in AI Agents and Full Stack Development.
-        I am looking for full-time opportunities in Software Engineering or Machine Learning where I can apply my skills and continue to grow as a developer.
+        I am looking for full-time opportunities in Software Engineering or Backend Engineering where I can apply my skills and continue to grow as a developer.
         The technologies I am most experienced with are{" "}
-        <InlineCode>Python</InlineCode>, <InlineCode>JavaScript</InlineCode>,{" "}
+        <InlineCode>Python</InlineCode>, <InlineCode>Java</InlineCode>,
+        <InlineCode>JavaScript</InlineCode>,{" "}
         <InlineCode>TypeScript</InlineCode>, <InlineCode>React</InlineCode>,{" "}
         <InlineCode>Next.js</InlineCode>, <InlineCode>Node.js</InlineCode>,{" "}
         <InlineCode>FastAPI</InlineCode>, <InlineCode>LangGraph</InlineCode>,{" "}
@@ -33,7 +34,7 @@ export const About = () => {
       <p>
         Outside of coding, I love to workout and stay active. I like to do calisthenics where my goal is to unlock as many skills as possible.
         I love to eat and try new foods and I am always looking for new restaurants to try. I cook occasionally and enjoy experimenting with new recipes.
-        I also really enjoy exploring new places and going on adventures. I was recently in Boston and had a great time exploring the city.
+        I also really enjoy exploring new places and going on adventures.
         Whenever I travel to somewhere new, I make it my mission to maximize my experience by trying local foods, visiting popular attractions, and immersing myself in the culture.
         Currently one of my interests is reading about Neuroscience. I find the brain to be fascinating and I love learning about how it works. This goes along with my love for learning where I try to be a little bit better every day or learn something new.
       </p>
